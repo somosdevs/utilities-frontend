4238e12bdc836f17b7c57d9ce81b704cf66b3e7c
Douglas Lovera <doulovera@gmail.com>
2021-10-31 21:14:54 -0500

    Add link and repoUrl to schema validation

--- schemas/projects.schema.js
+++ schemas/projects.schema.js
@@ -3,11 +3,15 @@ const Joi = require('joi')
 const name = Joi.string().min(3).max(30)
 const preview = Joi.string().uri()
 const languages = Joi.array().items(Joi.string()) // .valid() para validar los valores entrantes
+const link = Joi.string().uri()
+const repoUrl = Joi.string().uri()
 
 const createProductSchema = Joi.object({
   name: name.required(),
   preview: preview.optional(),
-  languages: languages.required()
+  languages: languages.required(),
+  link: link.required(),
+  repoUrl: repoUrl.optional()
 })
 
 export {
