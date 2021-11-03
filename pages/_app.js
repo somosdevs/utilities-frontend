339380a13f1a8257f60f11c39e909f4a6c7860d5
Douglas Lovera <doulovera@gmail.com>
2021-11-02 21:49:57 -0500

    Add progress bar to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,35 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+
 import '../styles/globals.css'
+import '../styles/nprogress.css'
 
 function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
+
+  // Progress Bar (NProgress)
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading ${url}`)
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.on('routeChangeStart', handleStart)
+      router.events.on('routeChangeComplete', handleStop)
+      router.events.on('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return <Component {...pageProps} />
 }
 
