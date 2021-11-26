06374843244966a658aa87ee5062baf8571e8a05
Douglas Lovera <doulovera@gmail.com>
2021-11-25 20:58:06 -0500

    fix: improve profile card style

--- components/ProfileCard/index.js
+++ components/ProfileCard/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import useUser from 'hooks/useUser'
 import CheckIcon from 'components/CheckIcon'
+import { Edit } from 'components/icons'
 
 export default function ProfileCard ({
   name = '',
@@ -7,9 +9,17 @@ export default function ProfileCard ({
   avatar = '',
   socialMedia = []
 }) {
+  const user = useUser()
+
   return (
-    <article className="bg-custLight-primary h-[410px] w-64 rounded-xl overflow-hidden group">
-      <div className="relative h-52 w-full">
+    <article className="relative bg-custLight-primary h-[320px] w-64 rounded-xl overflow-hidden group">
+      <button
+        className="absolute hidden items-center justify-center h-8 w-8 bg-cust-primary z-50 top-3 right-3 rounded-lg group-hover:flex"
+        onClick={() => console.log('edit')}
+      >
+        {name === user?.username ? <Edit fill="#fca311" /> : ''}
+      </button>
+      <div className="relative h-48 w-full">
         {
           avatar
             ? <Image
@@ -17,7 +27,7 @@ export default function ProfileCard ({
               alt={name}
               layout="fill"
               objectFit="cover"
-              className="object-center transition-transform duration-300 group-hover:scale-110"
+              className="object-center"
             />
             : <span className="square_loading"></span>
         }
@@ -25,15 +35,15 @@ export default function ProfileCard ({
       <div className="flex flex-col h-full p-3 max-h-[210px]">
         <div className="flex-auto">
           <h3 className="text-xl font-bold">{name}</h3>
-          <h5>{discord}</h5>
-          <div className="h-24 mt-5 text-sm">
+          <h5 className="opacity-80">{discord}</h5>
+          <div className="flex items-center justify-end gap-3 mt-5 pr-4 text-sm">
             {socialMedia.map((link) => (
               <a
                 key={link.name}
                 href={link.url}
                 target="_blank"
                 rel="noreferrer"
-                className=""
+                className="inline-block text-xl hover:opacity-60"
               >
                 <CheckIcon incomingIcon={link.name.toLowerCase()} />
               </a>
