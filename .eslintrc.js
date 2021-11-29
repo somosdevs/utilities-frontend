9a604b06e6661745fbe82ee719e8c7c845d890cf
Douglas Lovera <doulovera@gmail.com>
2021-11-28 21:00:26 -0500

    fix: set no unused vars rule as warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn'
   }
 }
--- pages/api/team/index.js
+++ pages/api/team/index.js
@@ -18,50 +18,50 @@ const Team = (req, res) => {
 
 export default Team
 
-const TEAM_MEMBERS = [
-  {
-    name: 'Christopher Glood',
-    discord: 'Consecutes#5009',
-    avatar: 'https://avatars.githubusercontent.com/u/36427177?v=4',
-    socialMedia: [
-      {
-        name: 'Github',
-        url: 'https://github.com/consecutes'
-      },
-      {
-        name: 'Twitter',
-        url: 'https://twitter.com/consecutes'
-      }
-    ],
-    technologies: []
-  },
-  {
-    name: 'Douglas Lovera',
-    discord: 'DouLo#0763',
-    avatar: 'https://avatars.githubusercontent.com/u/42481580?v=4',
-    socialMedia: [
-      {
-        name: 'Link',
-        url: 'https://doulovera.vercel.app/'
-      },
-      {
-        name: 'Github',
-        url: 'https://github.com/doulovera'
-      },
-      {
-        name: 'Twitter',
-        url: 'https://twitter.com/doulovera'
-      }
-    ],
-    technologies: [
-      'Javascript',
-      'Typescript',
-      'React.js',
-      'Next.js',
-      'Node.js'
-    ]
-  }
-]
+// const TEAM_MEMBERS = [
+//   {
+//     name: 'Christopher Glood',
+//     discord: 'Consecutes#5009',
+//     avatar: 'https://avatars.githubusercontent.com/u/36427177?v=4',
+//     socialMedia: [
+//       {
+//         name: 'Github',
+//         url: 'https://github.com/consecutes'
+//       },
+//       {
+//         name: 'Twitter',
+//         url: 'https://twitter.com/consecutes'
+//       }
+//     ],
+//     technologies: []
+//   },
+//   {
+//     name: 'Douglas Lovera',
+//     discord: 'DouLo#0763',
+//     avatar: 'https://avatars.githubusercontent.com/u/42481580?v=4',
+//     socialMedia: [
+//       {
+//         name: 'Link',
+//         url: 'https://doulovera.vercel.app/'
+//       },
+//       {
+//         name: 'Github',
+//         url: 'https://github.com/doulovera'
+//       },
+//       {
+//         name: 'Twitter',
+//         url: 'https://twitter.com/doulovera'
+//       }
+//     ],
+//     technologies: [
+//       'Javascript',
+//       'Typescript',
+//       'React.js',
+//       'Next.js',
+//       'Node.js'
+//     ]
+//   }
+// ]
 
 // export default function team (req, res) {
 //   res.status(200).json(TEAM_MEMBERS)
